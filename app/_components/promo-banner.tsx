e0f8243ef0395d17ea7666ebe22b249251ac0e96
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-05-01 23:45:54 -0300

    fix: fix alt and src props received by promobanner component

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -1,14 +1,20 @@
-import Image, { ImageProps } from "next/image";
+import Image from "next/image";
 
-const PromoBanner = (props: ImageProps) => {
+interface PromoBannerProps {
+  alt: string;
+  src: string;
+}
+
+const PromoBanner = ({ alt, src }: PromoBannerProps) => {
   return (
     <Image
+      alt={alt}
+      src={src}
       height={0}
       width={0}
       className="h-auto w-full object-contain"
       sizes="100vw"
       quality={100}
-      {...props}
     />
   );
 };
