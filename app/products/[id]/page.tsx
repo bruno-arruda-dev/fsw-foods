378f5a62b4ec77161b6981f81b374f80855e4d8e
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-05-04 23:05:21 -0300

    feat: add product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon } from "lucide-react";
-import Image from "next/image";
 import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-details";
 
 interface ProductPageProps {
   params: {
@@ -22,45 +21,26 @@ export default async function ProductPage({
     },
   });
 
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
   if (!product) {
     notFound();
   }
 
   return (
-    <div className="">
-      <div className="relative h-[360px] w-full">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-          className="object-cover"
-        />
+    <div>
+      <ProductImage product={product} />
 
-        <Button
-          className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
-          size="icon"
-        >
-          <ChevronLeftIcon />
-        </Button>
-      </div>
-
-      <div className="p-5">
-        <div className="flex items-center gap-[0.375rem] px-5">
-          <div className="relative h-6 w-6">
-            <Image
-              src={product.restaurant.imageUrl}
-              alt={product.restaurant.name}
-              fill
-              className="rounded-full object-cover"
-            />
-            <span className="whitespace-nowrap text-xs text-muted-foreground">
-              {product.restaurant.name}
-            </span>
-          </div>
-        </div>
-
-        <h1 className="mb-3 mt-1 text-xl font-semibold">{product.name}</h1>
-      </div>
+      <ProductDetails product={product} complementaryProducts={juices} />
     </div>
   );
 }
