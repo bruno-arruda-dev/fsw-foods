4577826e015f6fe12b3874f84f979b81c1681848
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-05-07 15:55:48 -0300

    feat: add category product page

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -4,6 +4,7 @@ import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { Badge } from "./ui/badge";
 import { ArrowDown } from "lucide-react";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -15,13 +16,17 @@ interface ProductItemProps {
       };
     };
   }>;
+  className?: string;
 }
 
-export default function ProductItem({ product }: ProductItemProps) {
+export default function ProductItem({ product, className }: ProductItemProps) {
   return (
-    <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+    <Link
+      className={cn("w-[150px] min-w-[150px]", className)}
+      href={`/products/${product.id}`}
+    >
       <div className="h-[250px] w-[150px] min-w-[150px] space-y-2">
-        <div className="relative h-[150px] w-full">
+        <div className="relative aspect-square w-full">
           <Image
             src={product.imageUrl}
             alt={product.name}
